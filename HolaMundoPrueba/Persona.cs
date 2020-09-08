2ed4d4fcc1015b169428d64f25cdbfef337f0954
mauricio1798 <70928418+mauricio1798@users.noreply.github.com>
2020-09-08 16:10:32 -0500

    Clase persona

---
+++ HolaMundoPrueba/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundoPrueba
+{
+    public class Persona
+    {
+        public string Nombre { set; get; }
+        public string Apellido { set; get; }
+
+        public Persona()
+        {
+
+        }
+
+        public Persona(string nombre, string apellido)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+    }
+}
